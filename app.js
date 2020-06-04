499648f594bb208d7fe32e89b3e9630986d43bcb
Giovanni-Rafael-Vuolo1 <giovanni.rafael.vuolo1@ibm.com>
2020-06-04 12:42:05 +0200

    last change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Let’s change from Hello from Appsody Demo! to Hello from Cloud Paks !!!");
+  res.send("Let’s change from Hello from Appsody Demo! to Hello from Cloud Paks !!! Last Changes!!!!!");
 });
  
 module.exports.app = app;
